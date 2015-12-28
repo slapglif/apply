6ae3e7babd1b6ee3f1c5c6bf621d495a7af49434
kuro112 <kuro@xtcr.net>
2015-12-27 19:26:37 -0700

    trying to fix shit

--- app/views.py
+++ app/views.py
@@ -200,15 +200,14 @@ def create_or_login(response):
     g.user = User.get_or_create(match.group(1))
     steamdata = get_steam_userinfo(g.user.steam_id)
     g.user.nickname = steamdata['personaname']
+    g.user.flag = 0
+    g.user.admin = 0
     for user1 in User.query.filter_by(steam_id=g.user.steam_id):
-        g.user.flag = 0
-        g.user.admin = 0
-        if g.user.flag and g.user.admin is not None:
-            g.user.flag = [user1][0].flag
-            g.user.admin = [user1][0].admin
-            if g.user.steam_id == "76561197966341176" or "STEAM_0:0:3037724":
-                g.user.admin = 1
-                g.user.flag = 2
+        g.user.flag = [user1][0].flag
+        g.user.admin = [user1][0].admin
+        if g.user.steam_id == "76561197966341176" or "STEAM_0:0:3037724":
+            g.user.admin = 1
+            g.user.flag = 2
 
     print "%s is admin %s is flag"%(g.user.admin,g.user.flag)
     db_session.commit()
